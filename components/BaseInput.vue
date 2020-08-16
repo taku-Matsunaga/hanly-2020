5c545bcb8761a11b51a8945036bc8c4628d449e8
Kiyohiko Heima <piyo326@gmail.com>
2020-08-16 14:38:54 +0900

    Add signup page

---
+++ components/BaseInput.vue
@@ -0,0 +1,54 @@
+<template>
+  <input
+    class="input"
+    :type="type"
+    :value="value"
+    :placeholder="placeholder"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.input {
+  display: block;
+  border: 1px solid transparent;
+  background: rgba(255, 255, 255, 0.2);
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 1.125rem;
+  padding: 0 1rem;
+  width: 100%;
+  height: 3.75rem;
+  line-height: 3.75rem;
+  border-radius: 4px;
+  & + & {
+    margin-top: 0.75rem;
+  }
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.2);
+  }
+  &:focus {
+    border: 1px solid #1bd7c7;
+    box-shadow: none;
+    outline: none;
+  }
+}
+</style>
---
+++ pages/signup/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrap">
+    <SignUpForm :is-sending="isSending" @submit="submit" />
+    <BaseButton to="/" class="mts" is-txt>戻る</BaseButton>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isSending: false,
+    }
+  },
+  methods: {
+    submit({ nickname, email, password }) {
+      // this.isSending を true にする
+      console.log(nickname, email, password)
+      // nickname, email, password を使ってサーバーにリクエスト
+      // this.isSending を false にする
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  padding: 2.5rem 1rem 0;
+}
+</style>
