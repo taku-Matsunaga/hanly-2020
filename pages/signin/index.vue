e069839340abd1bdd3916ec596a116aa5124fea1
Kiyohiko Heima <piyo326@gmail.com>
2020-08-16 15:03:58 +0900

    Add signin page

---
+++ pages/signin/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrap">
+    <SignInForm :is-sending="isSending" @submit="signIn" />
+    <BaseButton to="/" class="mts" is-txt>戻る</BaseButton>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isSending: false,
+    }
+  },
+  methods: {
+    signIn({ email, password }) {
+      // this.isSending を true にする
+      console.log(email, password)
+      // email, password を使ってサーバーにリクエスト
+      // this.isSending を false にする
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  padding: 2.5rem 1rem 0;
+}
+</style>
