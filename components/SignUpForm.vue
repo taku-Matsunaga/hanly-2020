6339b5c4debe5043df73e6955a2a291720c1ce60
taku-Matsunaga <skyline199306@yahoo.co.jp>
2021-02-26 12:17:13 +0900

    Add signup page

---
+++ components/SignUpForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <form
+    class="form"
+    @submit.prevent="$emit('submit', { nickname, email, password })"
+  >
+    <BaseInput v-model="nickname" placeholder="ニックネーム" />
+    <BaseInput v-model="email" placeholder="メールアドレス" type="email" />
+    <BaseInput v-model="password" placeholder="パスワード" type="password" />
+    <BaseButton class="mts" :disabled="isSending">新規登録</BaseButton>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    isSending: Boolean,
+  },
+  data() {
+    return {
+      nickname: '',
+      email: '',
+      password: '',
+    }
+  },
+}
+</script>
